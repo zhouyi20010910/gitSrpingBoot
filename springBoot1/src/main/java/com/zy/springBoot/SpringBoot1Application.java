5cff4299150c6abcd2c2bbc42e25c50da7af42f6
zhouyi <66905576+zhouyi20010910@users.noreply.github.com>
2020-06-15 17:42:37 +0800

    Update SpringBoot1Application.java

--- springBoot1/src/main/java/com/zy/springBoot/SpringBoot1Application.java
+++ springBoot1/src/main/java/com/zy/springBoot/SpringBoot1Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringBoot1Application {
-
+	//怎么没提交
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBoot1Application.class, args);
 	}
